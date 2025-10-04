63fd1c8519e322a68c298430378346e1c42e227d
StrawberryMilk21 <khochinning2002@gmail.com>
2025-10-04 18:09:49 +0800

    123

--- src/utils/geminiConfig.js
+++ src/utils/geminiConfig.js
@@ -2,7 +2,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // 直接硬编码 API Key
-const API_KEY = "AIzaSyCOlY2ZVZ8KWug5BOmwdWleVEYOZBzKh0g";
+const API_KEY = "AIzaSyD0KPoKjO19yPFZ1ojpDN0_uuZIcY3vDBE";
 
 export const genAI = new GoogleGenerativeAI(API_KEY);
 
